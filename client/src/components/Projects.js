a7a546904f0fbf056d2f2c269ef89b34f77257f3
Ankit Pavar <geekyanky@gmail.com>
2020-12-28 23:51:27 +0530

    Date format completed, Almost Completed

--- client/src/components/Projects.js
+++ client/src/components/Projects.js
@@ -44,19 +44,7 @@ const Projects = () => {
                 <p className='leading-relaxed text-base'>
                   {project.summary}
                 </p>
-                <button className='mt-3 text-indigo-500 inline-flex items-center'>
-                  {project.cost}
-                  <svg
-                    fill='none'
-                    stroke='currentColor'
-                    stroke-linecap='round'
-                    stroke-linejoin='round'
-                    stroke-width='2'
-                    className='w-4 h-4 ml-2'
-                    viewBox='0 0 24 24'>
-                    <path d='M5 12h14M12 5l7 7-7 7'></path>
-                  </svg>
-                </button>
+               
               </div>
             </div>
           </div>
--- client/src/pages/SuccessPage.js
+++ client/src/pages/SuccessPage.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import Moment from 'react-moment';
+import moment from 'moment';
 
 import { projectDetail } from '../actions/projectActions';
 
@@ -9,12 +11,23 @@ const SuccessPage = () => {
   const projectDetails = useSelector((state) => state.projectDetails);
   const { loading, error, project } = projectDetails;
 
+  let now = moment();
+  let d1 = now.format();
+  let d2 = moment(project.date);
+
+  let days = d2.diff(d1, 'days');
+
   React.useEffect(() => {
     dispatch(projectDetail());
   }, [dispatch]);
   return (
     <section className='text-gray-600 body-font'>
       <div className='container px-5 py-24 mx-auto'>
+        <div className='flex flex-col  w-full mb-20'>
+          <h1 className='sm:text-4xl text-3xl font-medium title-font text-blue-700'>
+            Success 🎉
+          </h1>
+        </div>
         <div className='flex flex-wrap -m-4'>
           <div className='p-4 md:w-1/2'>
             <div className='h-full w-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden'>
@@ -29,12 +42,16 @@ const SuccessPage = () => {
 
                 <span className='text-gray-400 mr-3 inline-flex items-center lg:ml-auto md:ml-0 ml-auto leading-none text-sm pr-3 py-1 border-r-2 border-gray-200'>
                   <box-icon name='calendar-week' color='#2222ee'></box-icon>
-                  {project.date}
+                  Delivery in {days} days{' '}
                 </span>
                 <span className='text-gray-400 inline-flex items-center leading-none text-sm'>
-                  <box-icon name='file-blank' color='#2222ee'></box-icon> Attachment
+                  <box-icon name='file-blank' color='#2222ee'></box-icon>{' '}
+                  Attachment
                 </span>
               </div>
+              <button className='w-36 p-2 rounded-md bg-blue-600 text-white m-4 ml-88'>
+                Cancel
+              </button>
             </div>
           </div>
         </div>
